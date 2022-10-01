2ea444e0c73d45ef2e4f2d6f42786802dd046d6a
JiYoul <wldbf97@naver.com>
2022-10-01 17:49:34 +0900

    [13일차] 스프링 타입 컨버터 다시 복습 겸 코딩 없어진거 다시 작성.

--- jiyoul/typeconverter/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ jiyoul/typeconverter/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // WebMvcConfigurer 를 구현해서 여기다가 다 등록해서 쓰면 WebConversionService 라는 곳에서 통합해서 관리하기 때문에 사용이 가능한 것.
     @Override
     public void addFormatters(FormatterRegistry registry) {
         //주석처리 우선순위
